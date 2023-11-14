9dffdc80e5721c9d65b51d4573c7ed9aa241b5cf
gregorywmorris <gwm1980@gmail.com>
2023-11-14 07:48:48 -0500

    main format update

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-
 from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env (especially openai api key)
+
+
+load_dotenv()  
 
 st.title("RockyBot: News Research Tool 📈")
 st.sidebar.title("News Article URLs")
